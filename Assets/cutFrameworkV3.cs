b87f543f9ef3f1ed4264117feebfc6e0ce8c5205
Brennan Schlittler <brennanschlittler@gmail.com>
2023-02-12 12:45:29 -0600

    Technically speaking this works

--- Assets/cutFrameworkV3.cs
+++ Assets/cutFrameworkV3.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class cutFrameworkV3 : MonoBehaviour
 {
-    [SerializeField] private bool[] crossGates;
+    private bool[] crossGates = new bool[2];
     [SerializeField] private GameObject cutSlice;
 
-    public void Start()
+    void Start()
     {
-        crossGates = new bool[3]; // dynamically get number of gates next time, okay?
+        Debug.Log("Starting!!");
+        crossGates = new bool[2]; // dynamically get number of gates next time, okay?
         for (int i = 0; i < crossGates.Length; i++)
             crossGates[i] = false;
     }
 
     public void gatePassed(int gateIndex)
     {
+        Debug.Log(gateIndex);
         if(gateIndex > 0)
         {
             if (!crossGates[gateIndex - 1])
             {
+                Debug.Log("Skipped a gate");
                 Restart();
                 return;
             }
