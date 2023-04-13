72d4e480b53b834c814284f80dcc3b448ed45922
Brennan Schlittler <brennanschlittler@gmail.com>
2023-04-13 12:34:23 -0500

    Updated Error Display

--- Assets/Scripts/DisplayError.cs
+++ Assets/Scripts/DisplayError.cs
@@ -10,11 +10,54 @@ public class DisplayError : MonoBehaviour
 
     public void updateResultsDisplay(List<CutRecord> results)
     {
-        for(int i = 0; i < transform.childCount; i++)
+        clearDisplay();
+        //oldDisplay(results);
+        newDisplay(results);
+
+        
+    }
+
+    private void clearDisplay()
+    {
+        for (int i = 0; i < transform.childCount; i++)
         {
             GameObject.Destroy(transform.GetChild(i).gameObject);
         }
+    }
 
+    private void newDisplay(List<CutRecord> results)
+    {
+        GameObject instantiated;
+        foreach (CutRecord cutRec in results)
+        {
+            instantiated = Instantiate(displayTemplate, transform);
+            string description = cutRec.getName() + ":";
+            instantiated.transform.GetChild(0).GetComponent<TMP_Text>().text = description;
+
+            List<float> teachErrors = cutRec.getTeachError();
+            List<float> testErrors = cutRec.getTestErrors();
+            int i = 0;
+            while (i < teachErrors.Count || i < testErrors.Count)
+            {
+                if (i < teachErrors.Count)
+                {
+                    instantiated = Instantiate(displayTemplate, transform);
+                    description = "    Teach Error " + (i + 1) + ": " + teachErrors[i].ToString("n2");
+                    instantiated.transform.GetChild(0).GetComponent<TMP_Text>().text = description;
+                }
+                if(i < testErrors.Count)
+                {
+                    instantiated = Instantiate(displayTemplate, transform);
+                    description = "    Test Error " + (i + 1) + ": " + testErrors[i].ToString("n2");
+                    instantiated.transform.GetChild(0).GetComponent<TMP_Text>().text = description;
+                }
+                i++;
+            }
+        }
+    }
+
+    private void oldDisplay(List<CutRecord> results)
+    {
         GameObject instantiated;
         foreach (CutRecord cutRec in results)
         {
@@ -23,11 +66,11 @@ public class DisplayError : MonoBehaviour
             List<float> teachErrors = cutRec.getTeachError();
             List<float> testErrors = cutRec.getTestErrors();
             int i = 0;
-            while(i < teachErrors.Count || i < testErrors.Count)
+            while (i < teachErrors.Count || i < testErrors.Count)
             {
-                if(i < teachErrors.Count && i < testErrors.Count)
+                if (i < teachErrors.Count && i < testErrors.Count)
                 {
-                    description = description + "Teach Error " + (i + 1) + ": " + teachErrors[i].ToString("n2") + " | Test Error " + (i+1) + ": " + testErrors[i].ToString("n2") + " \n";
+                    description = description + "Teach Error " + (i + 1) + ": " + teachErrors[i].ToString("n2") + " | Test Error " + (i + 1) + ": " + testErrors[i].ToString("n2") + " \n";
                 }
                 else if (i < teachErrors.Count && i >= testErrors.Count)
                 {
@@ -35,7 +78,7 @@ public class DisplayError : MonoBehaviour
                 }
                 else if (i >= teachErrors.Count && i < testErrors.Count)
                 {
-                    description = description + "                  "  + " | Test Error " + (i + 1) + ": " + testErrors[i].ToString("n2") + " \n";
+                    description = description + "                  " + " | Test Error " + (i + 1) + ": " + testErrors[i].ToString("n2") + " \n";
                 }
                 i++;
             }
